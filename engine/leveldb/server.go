7ab48e5d6356009941406ac2b6b30652d8529d27
wzshiming <wzshiming@foxmail.com>
2018-07-31 15:21:37 +0800

    add string method

---
+++ engine/leveldb/server.go
@@ -0,0 +1,15 @@
+package leveldb
+
+import (
+	"github.com/syndtr/goleveldb/leveldb"
+	"github.com/wzshiming/resp"
+)
+
+func (c *LevelDB) info(name string, args []resp.Reply) (resp.Reply, error) {
+	stats := &leveldb.DBStats{}
+	err := c.db.Stats(stats)
+	if err != nil {
+		return nil, err
+	}
+	return resp.Convert(stats), nil
+}
