524474ecefa5f136c2ad2cd14b77b1b58fc08f8f
wzshiming <wzshiming@foxmail.com>
2019-06-19 17:00:14 +0800

    add same

--- client/lrdb/lrdb.go
+++ client/lrdb/lrdb.go
@@ -1,6 +1,7 @@
 package lrdb
 
 import (
+	"errors"
 	"time"
 
 	"github.com/wzshiming/lrdb/reply"
@@ -39,6 +40,9 @@ func (c *Client) Execute(in, out interface{}) (err error) {
 	res, err := c.Cmd(req)
 	if err != nil {
 		return err
+	}
+	if re, ok := res.(resp.ReplyError); ok {
+		return errors.New(string(re))
 	}
 	if out != nil {
 		err = resp.ConvertFrom(res, out)
@@ -94,4 +98,78 @@ func (c *Client) Time() (t time.Time, err error) {
 // Info Returns server info
 func (c *Client) Info() (info *Info, err error) {
 	return info, c.Execute([]string{"info"}, &info)
+}
+
+// Set key to hold the string value.
+// If key already holds a value, it is overwritten, regardless of its type.
+func (c *Client) Set(k, v string) (err error) {
+	return c.Execute([]string{"set", k, v}, nil)
+}
+
+// Rename Renames key to newkey.
+// It returns an error when key does not exist.
+// If newkey already exists it is overwritten, when this happens RENAME executes an implicit DEL operation,
+// so if the deleted key contains a very big value it may cause high latency even
+// if RENAME itself is usually a constant-time operation.
+func (c *Client) Rename(k, nk string) (err error) {
+	return c.Execute([]string{"rename", k, nk}, nil)
+}
+
+// Get the value of key.
+// If the key does not exist the special value nil is returned.
+func (c *Client) Get(k string) (r string, err error) {
+	return r, c.Execute([]string{"get", k}, &r)
+}
+
+// GetSet Atomically sets key to value and returns the old value stored at key.
+func (c *Client) GetSet(k, v string) (r string, err error) {
+	return r, c.Execute([]string{"getset", k, v}, &r)
+}
+
+// Del Removes the specified keys. A key is ignored if it does not exist.
+func (c *Client) Del(k ...string) (d int, err error) {
+	return d, c.Execute(append([]string{"del"}, k...), &d)
+}
+
+// Exists Returns if key exists.
+// The user should be aware that if the same existing key is mentioned in the arguments multiple times,
+// it will be counted multiple times.
+// So if somekey exists, EXISTS somekey somekey will return 2.
+func (c *Client) Exists(k ...string) (d int, err error) {
+	return d, c.Execute(append([]string{"exists"}, k...), &d)
+}
+
+// SetBit Sets or clears the bit at offset in the string value stored at key.
+// The bit is either set or cleared depending on value.
+// When key does not exist, a new string value is created.
+// return original bit value stored at offset.
+func (c *Client) SetBit(k string, off int, f bool) (b bool, err error) {
+	return b, c.Execute([]interface{}{"setbit", off, b}, &b)
+}
+
+// GetBit Returns the bit value at offset in the string value stored at key.
+func (c *Client) GetBit(k string, off int) (b bool, err error) {
+	return b, c.Execute([]interface{}{"getbit", off}, &b)
+}
+
+// BitCount Returns the bit value at offset in the string value stored at key.
+func (c *Client) BitCount(k string) (b int, err error) {
+	return b, c.Execute([]interface{}{"bitcount", k}, &b)
+}
+
+// BitCountRange Like BitCount.
+// It is possible to specify the counting operation only in an interval passing the additional arguments start and end.
+func (c *Client) BitCountRange(k string, start uint, end uint) (b int, err error) {
+	return b, c.Execute([]interface{}{"bitcount", k, start, end}, &b)
+}
+
+// Append If key already exists and is a string, this command appends the value at the end of the string.
+// If key does not exist it is created and set as an empty string, so APPEND will be similar to SET in this special case.
+func (c *Client) Append(k string, v string) (b int, err error) {
+	return b, c.Execute([]string{"append", k, v}, &b)
+}
+
+// StrLen Returns the length of the string value stored at key.
+func (c *Client) StrLen(k string) (b int, err error) {
+	return b, c.Execute([]string{"strlen", k}, &b)
 }
