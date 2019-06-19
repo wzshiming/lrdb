d2d01e52c5c05432bdb34e9d4c94b90c823f24a2
wzshiming <wzshiming@foxmail.com>
2019-06-19 10:50:26 +0800

    clear up

--- engine/engine.go
+++ engine/engine.go
@@ -2,17 +2,10 @@ package engine
 
 import (
 	"errors"
-
-	"github.com/wzshiming/resp"
 )
 
 var (
 	ErrWrongNumberOfArguments = errors.New("Error wrong number of arguments")
 	ErrUnsupportedForm        = errors.New("Error unsupported form")
 	ErrEmptyData              = errors.New("Error empty data")
-)
-
-var (
-	OK   = resp.ReplyStatus("OK")
-	PONG = resp.ReplyStatus("PONG")
 )
