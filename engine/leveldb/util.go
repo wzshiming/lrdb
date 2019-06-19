fc1b76386ffb2cf24176d4af17e4f324ff4e82ef
wzshiming <wzshiming@foxmail.com>
2019-06-19 15:38:19 +0800

    fixes

--- engine/leveldb/util.go
+++ engine/leveldb/util.go
@@ -70,20 +70,20 @@ func getBit(i int) byte {
 	default:
 		return 0
 	case 0:
-		return 1 << 0
-	case 1:
-		return 1 << 1
-	case 2:
-		return 1 << 2
-	case 3:
-		return 1 << 3
-	case 4:
-		return 1 << 4
-	case 5:
-		return 1 << 5
-	case 6:
-		return 1 << 6
-	case 7:
 		return 1 << 7
+	case 1:
+		return 1 << 6
+	case 2:
+		return 1 << 5
+	case 3:
+		return 1 << 4
+	case 4:
+		return 1 << 3
+	case 5:
+		return 1 << 2
+	case 6:
+		return 1 << 1
+	case 7:
+		return 1 << 0
 	}
 }
