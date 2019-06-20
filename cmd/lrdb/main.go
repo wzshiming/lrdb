d71de3e9c0926c7fa48f7294a0bb04ac389e0369
wzshiming <wzshiming@foxmail.com>
2019-06-20 12:30:15 +0800

    add pprof

--- cmd/lrdb/main.go
+++ cmd/lrdb/main.go
@@ -2,17 +2,24 @@ package main
 
 import (
 	"flag"
+	"net/http"
+	_ "net/http/pprof"
 
 	"github.com/wzshiming/lrdb"
 	"github.com/wzshiming/lrdb/engine/leveldb"
 	"gopkg.in/ffmt.v1"
 )
 
+var debug = flag.String("debug", ":10010", "debug port")
 var port = flag.String("port", ":10008", "Listen port")
 var path = flag.String("path", "./data", "Data path")
 
 func main() {
+
 	flag.Parse()
+	if *debug != "" {
+		go http.ListenAndServe(*debug, nil)
+	}
 	db, err := leveldb.NewLevelDB(*path)
 	if err != nil {
 		ffmt.Mark(err)
