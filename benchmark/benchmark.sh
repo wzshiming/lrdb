cdcc7d263c6d65241263a52a647e2826a994f58a
wzshiming <wzshiming@foxmail.com>
2019-06-21 14:59:52 +0800

    add ssdb benchmark

--- benchmark/benchmark.sh
+++ benchmark/benchmark.sh
@@ -2,16 +2,24 @@
 
 echo "Start redis benchmark ... "
 docker run --rm -d --name lrdb-redis redis:alpine redis-server
-docker run --rm -it --link lrdb-redis:lrdb-redis redis:alpine redis-benchmark -h lrdb-redis -p 6379 -q -c 50 -n 100000 -t ping,set,get
-docker run --rm -it --link lrdb-redis:lrdb-redis redis:alpine redis-benchmark -h lrdb-redis -p 6379 -q -c 50 -n 100000 -t ping,set,get -P 4
-docker run --rm -it --link lrdb-redis:lrdb-redis redis:alpine redis-benchmark -h lrdb-redis -p 6379 -q -c 50 -n 100000 -t ping,set,get -P 8
+docker run --rm -it --link lrdb-redis:lrdb-redis redis:alpine redis-benchmark -h lrdb-redis -p 6379 -q -c 50 -n 100000 -t set,get
+docker run --rm -it --link lrdb-redis:lrdb-redis redis:alpine redis-benchmark -h lrdb-redis -p 6379 -q -c 50 -n 100000 -t set,get -P 4
+docker run --rm -it --link lrdb-redis:lrdb-redis redis:alpine redis-benchmark -h lrdb-redis -p 6379 -q -c 50 -n 100000 -t set,get -P 8
 docker kill lrdb-redis
 echo "Finished redis benchmark"
 
+echo "Start ssdb benchmark ... "
+docker run --rm -d --name lrdb-ssdb wzshiming/ssdb
+docker run --rm -it --link lrdb-ssdb:lrdb-ssdb redis:alpine redis-benchmark -h lrdb-ssdb -p 8888 -q -c 50 -n 100000 -t set,get
+docker run --rm -it --link lrdb-ssdb:lrdb-ssdb redis:alpine redis-benchmark -h lrdb-ssdb -p 8888 -q -c 50 -n 100000 -t set,get -P 4
+docker run --rm -it --link lrdb-ssdb:lrdb-ssdb redis:alpine redis-benchmark -h lrdb-ssdb -p 8888 -q -c 50 -n 100000 -t set,get -P 8
+docker kill lrdb-ssdb
+echo "Finished ssdb benchmark"
+
 echo "Start lrdb benchmark ... "
 docker run --rm -d --name lrdb-lrdb wzshiming/lrdb
-docker run --rm -it --link lrdb-lrdb:lrdb-lrdb redis:alpine redis-benchmark -h lrdb-lrdb -p 10008 -q -c 50 -n 100000 -t ping,set,get
-docker run --rm -it --link lrdb-lrdb:lrdb-lrdb redis:alpine redis-benchmark -h lrdb-lrdb -p 10008 -q -c 50 -n 100000 -t ping,set,get -P 4
-docker run --rm -it --link lrdb-lrdb:lrdb-lrdb redis:alpine redis-benchmark -h lrdb-lrdb -p 10008 -q -c 50 -n 100000 -t ping,set,get -P 8
+docker run --rm -it --link lrdb-lrdb:lrdb-lrdb redis:alpine redis-benchmark -h lrdb-lrdb -p 10008 -q -c 50 -n 100000 -t set,get
+docker run --rm -it --link lrdb-lrdb:lrdb-lrdb redis:alpine redis-benchmark -h lrdb-lrdb -p 10008 -q -c 50 -n 100000 -t set,get -P 4
+docker run --rm -it --link lrdb-lrdb:lrdb-lrdb redis:alpine redis-benchmark -h lrdb-lrdb -p 10008 -q -c 50 -n 100000 -t set,get -P 8
 docker kill lrdb-lrdb
 echo "Finished lrdb benchmark"
