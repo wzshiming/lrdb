6f6af884dcfc1951f3a90ec84b0adc44faa08cfd
wzshiming <wzshiming@foxmail.com>
2019-06-21 15:29:48 +0800

    update benchmark

--- benchmark/benchmark.sh
+++ benchmark/benchmark.sh
@@ -1,25 +1,42 @@
 #!/bin/sh
 
-echo "Start redis benchmark ... "
-docker run --rm -d --name lrdb-redis redis:alpine redis-server
-docker run --rm -it --link lrdb-redis:lrdb-redis redis:alpine redis-benchmark -h lrdb-redis -p 6379 -q -c 50 -n 100000 -t set,get
-docker run --rm -it --link lrdb-redis:lrdb-redis redis:alpine redis-benchmark -h lrdb-redis -p 6379 -q -c 50 -n 100000 -t set,get -P 4
-docker run --rm -it --link lrdb-redis:lrdb-redis redis:alpine redis-benchmark -h lrdb-redis -p 6379 -q -c 50 -n 100000 -t set,get -P 8
-docker kill lrdb-redis
-echo "Finished redis benchmark"
+if [[ -z "${DB_ARGS}" ]]; then
+    DB_ARGS="redis:alpine;6379 wzshiming/ssdb;8888 wzshiming/lrdb;10008"
+fi
 
-echo "Start ssdb benchmark ... "
-docker run --rm -d --name lrdb-ssdb wzshiming/ssdb
-docker run --rm -it --link lrdb-ssdb:lrdb-ssdb redis:alpine redis-benchmark -h lrdb-ssdb -p 8888 -q -c 50 -n 100000 -t set,get
-docker run --rm -it --link lrdb-ssdb:lrdb-ssdb redis:alpine redis-benchmark -h lrdb-ssdb -p 8888 -q -c 50 -n 100000 -t set,get -P 4
-docker run --rm -it --link lrdb-ssdb:lrdb-ssdb redis:alpine redis-benchmark -h lrdb-ssdb -p 8888 -q -c 50 -n 100000 -t set,get -P 8
-docker kill lrdb-ssdb
-echo "Finished ssdb benchmark"
+if [[ -z "${COMMAND_ARGS}" ]]; then
+    COMMAND_ARGS="SET GET"
+fi
 
-echo "Start lrdb benchmark ... "
-docker run --rm -d --name lrdb-lrdb wzshiming/lrdb
-docker run --rm -it --link lrdb-lrdb:lrdb-lrdb redis:alpine redis-benchmark -h lrdb-lrdb -p 10008 -q -c 50 -n 100000 -t set,get
-docker run --rm -it --link lrdb-lrdb:lrdb-lrdb redis:alpine redis-benchmark -h lrdb-lrdb -p 10008 -q -c 50 -n 100000 -t set,get -P 4
-docker run --rm -it --link lrdb-lrdb:lrdb-lrdb redis:alpine redis-benchmark -h lrdb-lrdb -p 10008 -q -c 50 -n 100000 -t set,get -P 8
-docker kill lrdb-lrdb
-echo "Finished lrdb benchmark"
+if [[ -z "${PIPELINE_SIZE_ARGS}" ]]; then
+    PIPELINE_SIZE_ARGS="1 2 4 8 16"
+fi
+
+if [[ -z "${CLIENT_SIZE_ARGS}" ]]; then
+    CLIENT_SIZE_ARGS="50 100 200"
+fi
+
+if [[ -z "${REQUEST_SIZE_ARGS}" ]]; then
+    REQUEST_SIZE_ARGS="100000 1000000"
+fi
+
+echo "Start benchmark ... "
+for COMMAND in ${COMMAND_ARGS[@]}; do
+    for PIPELINE_SIZE in ${PIPELINE_SIZE_ARGS[@]}; do
+        for CLIENT_SIZE in ${CLIENT_SIZE_ARGS[@]}; do
+            for REQUEST_SIZE in ${REQUEST_SIZE_ARGS[@]}; do
+                for DB in ${DB_ARGS[@]}; do
+                    DB_IMAGE=${DB%;*}
+                    DB_PORT=${DB#*;}
+                    docker run --rm -d --name lrdb-tmp-benchmark $DB_IMAGE > /dev/null 2>&1
+                    echo "Image:$DB_IMAGE;	Test:$COMMAND;	Clients:$CLIENT_SIZE;	Requests:$REQUEST_SIZE;	Pipeline:$PIPELINE_SIZE;"
+                    docker run --rm -it --link lrdb-tmp-benchmark:lrdb-tmp-benchmark redis:alpine \
+                        redis-benchmark -h lrdb-tmp-benchmark -p $DB_PORT -q \
+                        -t $COMMAND -c $CLIENT_SIZE -n $REQUEST_SIZE -P $PIPELINE_SIZE
+                    docker kill lrdb-tmp-benchmark > /dev/null 2>&1
+                done
+            done
+        done
+    done
+done
+echo "Finished benchmark."
